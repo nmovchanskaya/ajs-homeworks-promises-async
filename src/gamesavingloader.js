1f02f0cea4b60aed0556908065b8b841ab9a2dc9
nika.movchanskaya <nika.movchanskaya@gmail.com>
2023-06-27 14:24:15 +0200

    initial commit

--- src/gamesavingloader.js
+++ src/gamesavingloader.js
@@ -3,30 +3,17 @@ import json from './parser.js';
 import GameSaving from './gamesaving.js';
 
 export default class GameSavingLoader {
-  constructor() {
-    this.game = new GameSaving();
-  }
-
   static async load() {
-    let data = null;
-    let dataParsed = null;
-
     try {
-      data = await read();
+      const data = await read();
+      const dataParsed = await json(data);
+      const { id, created, userInfo } = JSON.parse(dataParsed);
+      return new GameSaving(id, created, userInfo);
     } catch (e) {
       console.log(`error: ${e}`);
-      return this.game;
+      return e;
     }
-    try {
-      dataParsed = await json(data);
-    } catch (e) {
-      console.log(`error: ${e}`);
-      return this.game;
-    }
-    const obj = JSON.parse(dataParsed);
-    this.game = new GameSaving(obj.id, obj.created, obj.userInfo);
-    return this.game;
   }
 }
 
-let obj = GameSavingLoader.load();
+const obj = GameSavingLoader.load();
